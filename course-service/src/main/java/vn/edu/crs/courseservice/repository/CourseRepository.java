194f2430dd8a66728342e025a9e4fa4caf4b6f02
Admin <Admin@MINH>
2026-08-06 08:42:00 +0700

    feat(course-service): CRUD course 3-layer + DTO

---
+++ course-service/src/main/java/vn/edu/crs/courseservice/repository/CourseRepository.java
@@ -0,0 +1,10 @@
+package vn.edu.crs.courseservice.repository;
+
+import vn.edu.crs.courseservice.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    boolean existsByTenMonHocIgnoreCase(String tenMonHoc);
+}
